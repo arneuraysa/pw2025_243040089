a5836f24b4ef89f0949b30fa710cefba6abf4d0d
arneuraysa <arneuraysa@gmail.com>
2025-02-16 20:45:53 +0700

    Menyelesaikan Tugas

---
+++ tugas/tugas1/tugas1c.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1c</title>
+    <style>
+        .kotak {
+            display: inline-block;
+            width: 50px;
+            height: 50px;
+            border: 1px solid;
+            background-color: pink;
+            text-align: center;
+            font-size: 20px;
+            margin-left: -5px;
+        }
+    </style>
+</head>
+<body>
+    <div class="kotak">1</div></br>
+    <div class="kotak">2</div>
+    <div class="kotak">2</div></br>
+    <div class="kotak">3</div>
+    <div class="kotak">3</div>
+    <div class="kotak">3</div>
+</body>
+</html>
