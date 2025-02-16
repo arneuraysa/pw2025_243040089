de66897f63a558d553c3f336d81d04d97f22797e
arneuraysa <arneuraysa@gmail.com>
2025-02-16 18:32:17 +0700

    main

---
+++ kuliah/pertemuan2/latihan1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba PHP</title>
+</head>
+<body>
+
+    <h1><?php echo "Hello World"; ?></h1>
+
+    <h4>Nama Saya : <?php echo "Arneu Raysa" ?></h4>
+
+</body>
+</html>
---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,14 @@
+<?php
+
+    $nama = "Arneu Raysa";
+    $email = "arneuraysa@gmail.com";
+    $pekerjaan = "Atlet";
+    $universitas = "Universitas Pasundan";
+
+
+    echo "<h1>$nama</h1>";
+    echo "<p>$email</p>";
+    echo "<p>$pekerjaan</p>";
+    echo "Hallo, nama saya $nama, saya adalah mahasiswa Teknik Informatika $universitas";
+
+?>
